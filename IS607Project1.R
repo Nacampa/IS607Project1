a8e0c741bd0d5b00f2a8dd752165c44d67c46a37
Nacampa <Nacampa@users.noreply.github.com>
2014-08-31 19:10:46 -0400

    Commit week 1 assignment IS607

---
+++ IS607Project1.R
@@ -0,0 +1,8 @@
+# 1 - R version: 3.0.2 R Studio version: 0.98.501
+> # 2 - PostgreSQL version: 9.3
+  > # 3 - Package DMwR is loaded, data set sales is loaded
+  > #   The number of observations is displayed 
+  > library(DMwR)
+data(sales)
+head(sales)
+nrow(sales)
